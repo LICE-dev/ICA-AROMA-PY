157dfab7d08da44d97232375325cbe2aa9050c0c
Agostino_Arcasensa <powerictsoft@gmail.com>
2026-01-08 16:00:02 +0100

    MANIFEST.in update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ica-aroma-py",
-    version="0.1.0",
+    version="0.1.1",
     description="ICA-AROMA packaged for Python import usage.",
     packages=find_packages(),
     include_package_data=True,
